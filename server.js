6567157157614a1e84514c28a0e103979b38d780
page hunt <pagelhunt@gmail.com>
2020-05-30 19:58:09 -0600

    heroku stuff

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
-// mongoose.connect("mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/api.js"));
